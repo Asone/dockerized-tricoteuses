545384d0c4c905562e22856b004a7f57603684da
Asone <nelson@herbin.info>
2018-07-30 01:12:38 +0200

    add db auto-install

--- _provisioning/db/init/03-import-dump.sh
+++ _provisioning/db/init/03-import-dump.sh
@@ -1,33 +1,10 @@
 #!/bin/bash
 set -e
 
+dropdb -U user ameli 
+createdb -U user -E utf-8 ameli
+
 cd /_dumps
 wget http://data.senat.fr/data/ameli/ameli.zip -O ameli.zip && \
-unzip ameli.zip && \
-ls && \ 
-psql ameli < ./var/opt/opendata/ameli.sql
-#cd var/opt/opendata \ 
-su postgres \
-dropdb ameli \
-dropuser opendata \
-createuser -IPRS opendata \
-createdb -E utf-8 -O ameli ameli \
-exit \
-
-#psql ameli < ./ameli.sql
-#su \
-#su postgres \
-#ropdb ameli \
-#dropuser opendata \
-#createuser -IPRS opendata \
-#   Enter password for new role: opendata
-#   Enter it again: opendata
-#createdb -E utf-8 -O ameli ameli \
-#psql ameli < /_dumps/ameli.sql \
-#exit \
-#rm -R var/ \ 
-#rm ameli.zip;
-
-#if [ -f /_dumps/ameli.sql ]; then
-
-#fi
+unzip -o ameli.zip && \
+psql -U user ameli < var/opt/opendata/ameli.sql 
